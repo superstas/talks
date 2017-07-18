176ac7ff6274d13f61bd9c86a48c32e05b923edb
Stanislav Afanasev <safanasev@gojuno.com>
2017-07-18 23:25:51 +0300

    improvments

--- 2017/pipelines_in_go/misc/reader.c
+++
@@ -1,6 +0,0 @@
-/**
- * Analogue for better understanding
- * read()  attempts  to read up to count bytes from file descriptor fd into
- * the buffer starting at buf.
- */
-ssize_t read(int fd, void *buf, size_t count);
