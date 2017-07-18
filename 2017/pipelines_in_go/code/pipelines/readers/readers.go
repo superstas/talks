44aa9f22f605372a8a9da213c6636ab45986b6de
Afanasev Stanislav <phpprogger@gmail.com>
2017-07-18 08:12:02 +0300

    85% done

--- 2017/pipelines_in_go/code/pipelines/readers/readers.go
+++ 2017/pipelines_in_go/code/pipelines/readers/readers.go
@@ -2,10 +2,10 @@ package readers
 
 import "io"
 
-
 type simpleReader struct {
 	data []byte
 }
+
 // 1 OMIT
 func SimpleReader(s string) *simpleReader {
 	return &simpleReader{[]byte(s)}
@@ -13,20 +13,18 @@ func SimpleReader(s string) *simpleReader {
 
 func (r *simpleReader) Read(p []byte) (int, error) {
 	n := copy(p, r.data) // HL
-	if n == 0 {
-		return 0, io.ErrUnexpectedEOF
-	}
-	r.data = r.data[:0] // HL
+	r.data = r.data[:0]  // HL
 	return n, io.EOF
 }
+
 // END 1 OMIT
 
 // 3 OMIT
 func (r *simpleReader) WriteTo(w io.Writer) (int, error) {
 	return w.Write(r.data)
 }
-// END 3 OMIT
 
+// END 3 OMIT
 
 type brokenReader struct {
 	io.Reader
@@ -50,4 +48,5 @@ func (r *brokenReader) Read(p []byte) (int, error) {
 	}
 	return n, err
 }
+
 // END 2 OMIT
